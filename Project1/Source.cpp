e5ff7aa5c2dd5c80dbbe8b4c32041e58108b9438
Александр Галюк <mr.bochka003@gmail.com>
2020-04-02 12:44:27 +1000

    Ввод числа тест

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,1 +1,12 @@
-fjghdshgdfgidsf
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	int i;
+	char* string1;
+	cin >> string1;
+	cout << "\n";
+	for (i = 0; i < strlen(string1); i++)
+		cout << string1[i] << "\n";
+}
