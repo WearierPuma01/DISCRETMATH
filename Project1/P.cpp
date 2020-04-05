e4f9a8f765516bb568f444277e1d0697c8aeff21
Рашид Сафаров <peccasim@yandex.ru>
2020-04-06 01:50:07 +0300

    Revert "ADD_PP_P начало положено не ругайся насяльника, я в гите запутался"
    This reverts commit 7f25991406f2b76769fe877a084d83961ddbc1a8.

--- Project1/P.cpp
+++ Project1/P.cpp
@@ -1,18 +1,1 @@
-#include<iostream>
-#include"P.h"
-using namespace std;
-
-Poli ADD_PP_P(Poli p1, Poli p2)
-{
-	Poli pRez;
-
-
-
-
-
-
-	for (int i = 0; i < deg + 1; i++)
-	{
-		pRez.coeff[i] = ADD_QQ_Q(p1.coeff[i], p2.coeff[i]);
-	}
-}
+safsa
