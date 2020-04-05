563a27a03cf8cc861185b4d57280f9aad3e6d2df
Рашид Сафаров <peccasim@yandex.ru>
2020-04-06 01:52:57 +0300

    исправил структуру полинома, добавил комменты

--- Project1/P.h
+++ Project1/P.h
@@ -1,6 +1,7 @@
 #pragma once
 struct Poli
 {
-	int deg;
-	float coeff[];
+	int deg;// степень многочлена
+	int* Cnumo = new int[deg + 1];// числители рациональной дроби коэффициентов
+	int* Cdeno = new int[deg + 1];// знаменатели рациональной дроби коэффициантов
 };
